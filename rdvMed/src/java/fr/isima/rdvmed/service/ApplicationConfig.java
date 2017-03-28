21d2b758d937e69be787585ee7ea7490abddff20
Benoît <benoit.garcon@outlook.com>
2017-03-28 16:55:27 +0200

    retour des creneaux

--- rdvMed/src/java/fr/isima/rdvmed/service/ApplicationConfig.java
+++ rdvMed/src/java/fr/isima/rdvmed/service/ApplicationConfig.java
@@ -29,6 +29,7 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(fr.isima.rdvmed.service.CreneauxFacadeREST.class);
         resources.add(fr.isima.rdvmed.service.MedecinsFacadeREST.class);
         resources.add(fr.isima.rdvmed.service.PatientsFacadeREST.class);
         resources.add(fr.isima.rdvmed.service.RdvFacadeREST.class);
---
+++ rdvMed/src/java/fr/isima/rdvmed/service/CreneauxFacadeREST.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.isima.rdvmed.service;
+
+import fr.isima.rdvmed.Creneaux;
+import fr.isima.rdvmed.ejb.CreneauxEJB;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author vagrant
+ */
+@Stateless
+@Path("creneaux")
+public class CreneauxFacadeREST {
+
+    @EJB
+    private CreneauxEJB creneaux;
+
+    @POST    
+    @Consumes({MediaType.APPLICATION_JSON})
+    public void create(Creneaux entity) {
+        creneaux.create(entity);
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes({MediaType.APPLICATION_JSON})
+    public void edit(@PathParam("id") Short id, Creneaux entity) {
+        creneaux.edit(entity);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void remove(@PathParam("id") Short id) {
+        creneaux.remove(creneaux.find(id));
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public Creneaux find(@PathParam("id") Short id) {
+        return creneaux.find(id);
+    }
+
+    @GET    
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Creneaux> findAll() {
+        return creneaux.findAll();
+    }
+
+    @GET
+    @Path("{from}/{to}")
+    @Produces({MediaType.APPLICATION_JSON})
+    public List<Creneaux> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        return creneaux.findRange(new int[]{from, to});
+    }
+
+    @GET
+    @Path("count")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String countREST() {
+        return String.valueOf(creneaux.count());
+    }
+
+}
