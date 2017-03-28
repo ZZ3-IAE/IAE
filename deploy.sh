59c3e7b5b9f216d1991bd03dbbb7ec743ecbe98e
Benoît <benoit.garcon@outlook.com>
2017-03-28 13:57:13 +0200

    deploy 5

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # updating system only if needed
-# apt-get update -y -qq
+apt-get update -y -qq
 # apt-get upgrade -y -qq
 # installing java jdk 8
 apt-get install -y oraclejdk8 -qq
@@ -15,10 +15,18 @@ apt-get install -y git -qq
 apt-get install -y firefox -qq
 # downloading project
 git clone https://github.com/begarco/IAE.git
+echo "après clone"
+pwd
+ls -l
 # installing payara
 cp -r IAE/matos/payara41 .
+echo "copie de payara"
+pwd
+ls -l
 # driver jdbc
 cp IAE/matos/mysql-jdbc.jar payara41/glasfish/lib
+# creation of database
+mysql -u root --password=iae2016 < IAE/matos/script.sql
 # starting payara
 payara41/bin/asadmin start-domain
 # creating a connexion pool
@@ -31,5 +39,6 @@ payara41/bin/asadmin deploy IAE/rdvMed/dist/rdvMed.war
 # launching demo
 #firefox localhost:8080/rdvMed
 # installing payara
-cp IAE/test.sh .
+cp IAE/matos/test.sh .
+chmod a+rwx test.sh
 ./test.sh
